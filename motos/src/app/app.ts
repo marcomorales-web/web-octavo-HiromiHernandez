46b13a22c54e98d8fe1a3c5b46de3fe6f64f5f83
MarcoMorales-web <marco.morales@itsvc.edu.mx>
2026-02-11 16:00:11 -0600

    Correcion Hiromi

--- motos/src/app/app.ts
+++ motos/src/app/app.ts
@@ -3,9 +3,10 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true, // 👈 FALTABA ESTO
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('motos');
